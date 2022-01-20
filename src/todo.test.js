58d3d5ace77d9bb2da52f172e3312a03c6d08c50
VuDej <93735137+VuDej@users.noreply.github.com>
2022-01-20 15:25:30 +0100

    added test for edit and status

--- src/todo.test.js
+++ src/todo.test.js
@@ -34,3 +34,33 @@ describe('remove all completed task', () => {
     expect(todo.todos.length).toBe(2);
   });
 });
+
+describe('edit todo', () => {
+  const index = 1;
+  it('should change todo description from test-2 to hello', () => {
+    todo.editTodo(index, 'hello');
+    expect(todo.todos.map((item) => {
+      if (item.index === index) {
+        return item.description;
+      }
+    }).join('')).toBe('hello');
+  });
+  it('should toggle status from false to true', () => {
+    const index = 1;
+    todo.isCompleted(index);
+    expect(todo.todos.map((item) => {
+      if (item.index === index) {
+        return item.completed;
+      }
+    }).join('')).toBeTruthy();
+  });
+  it('should toggle status from true to false', () => {
+    const index = 1;
+    todo.isCompleted(index);
+    expect(todo.todos.map((item) => {
+      if (item.index === Number(index)) {
+        return item.completed;
+      }
+    }).join('')).toMatch('false');
+  });
+});
