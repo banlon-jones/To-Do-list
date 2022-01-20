b29d0d0f2cbc8d32d230bb044bd2b921811e94b2
banlonjones <banlonjones@go-groups.net>
2022-01-20 08:59:03 +0100

    add todo test added

---
+++ src/todo.test.js
@@ -0,0 +1,18 @@
+import Todo from './todo';
+import LocalStorage from './local-storage-mock';
+
+global.localStorage = new LocalStorage();
+const todo = new Todo();
+
+describe('Add todo test', () => {
+  test('adding todo', () => {
+    todo.addTodo('test-1');
+    expect(todo.todos.length).toBe(1);
+  });
+
+  test('check description', () => {
+    todo.addTodo('test-2');
+    expect(todo.todos[1].description).toBe('test-2');
+  });
+});
+
